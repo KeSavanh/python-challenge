7e7b44f54121a8d4ce142f02817356a2473318e7
Vilaysack Khonsavanh <vilaysackkhonsavanh@Vilaysacks-MacBook-Pro.local>
2021-08-23 21:49:20 -0700

    worked and fixed total month count

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,10 +6,13 @@ budget_csv = os.path.join('budget_data.csv')
 with open(budget_csv, 'r') as budget:
     budget_reader = csv.reader(budget, delimiter=",")
     next (budget_reader, None)
-    #Total_month = 0
+    Total_month = 0
     total_budget = 0
     for row in budget_reader:
-        Date = [row[0]]
-        #Total_month = 
-        #total_budget += int(row[1])
-        print(Total_month)
+        Total_month += 1
+        total_budget += int(row[1])
+    print ("Financial Analysis")
+    print ("-----------------------------")
+    print("Total Months:" + " " + str(Total_month))
+    print("Total:" + " " + "$" + str(total_budget))
+    #Print ("Average Change:" + " " + )
